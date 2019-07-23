d247aae00559b3ab3a602da7d704763e3311dbdc
Rizvanolli <R66579@axafinancialsvc.axa-financial.intraxa>
2019-07-23 16:47:30 -0400

    added image to chatbot

--- root/scripts/simpleChatbot.js
+++ root/scripts/simpleChatbot.js
@@ -9,7 +9,7 @@ function bot() {
     	console.log(input);
 
 	if (questionNum == 0) {
-    		output.innerHTML = '<h1>My name is Edison and I help with ideation.</h1>'; // output response
+    		output.innerHTML = '<h1>My name is Edison and I help ideate.</h1>'; // output response
     		document.getElementById("input").value = ""; // clear text box
     		question = '<h1>How is your project going?</h1>'; // load next question		
     		setTimeout(timedQuestion, 2000); // output next question after 2sec delay
@@ -18,7 +18,7 @@ function bot() {
     		if (input.includes("good")) {
 			output.innerHTML = '<h1>I\'m glad to hear that!</h1>';
     			document.getElementById("input").value = "";   
-    			question = '<h1>Is there room for improvement?</h1>';					      	
+    			question = '<h1>Do you know how you could improve it?</h1>';					      	
     			setTimeout(timedQuestion, 2000);
 		}
 		else if (input.includes("bad")) {
@@ -30,17 +30,16 @@ function bot() {
     	}
     	else if (questionNum == 2) {
     		if (input.includes("yes")) {
-			output.innerHTML = '<h1>Here\'s a book that I recommend: </h1><a href="https://www.amazon.com/Improvement-Guide-Practical-Organizational-Performance/dp/0470192410">Book</a>';
+			output.innerHTML = '<h1>Here\'s a book that I recommend:</h1><img src="https://www.amazon.com/Improvement-Guide-Practical-Organizational-Performance/dp/0470192410">';
     			document.getElementById("input").value = "";   
     			question = '<h1>Was that helpful?</h1>';					      	
     			setTimeout(timedQuestion, 2000);
 		}
 		else if (input.includes("no")) {
-			output.innerHTML = '<h1>Check out this image to make sure.</h1>';
+			output.innerHTML = '<h1>This guide helps you consider other possibilities.</h1>';
     			setTimeout(timedQuestion, 2000);
-			window.open("http://images2.minutemediacdn.com/image/upload/c_fit,f_auto,fl_lossy,q_auto,w_728/v1555916687/shape/mentalfloss/cognitive_biases.png");
 			document.getElementById("input").value = "";   
-    			question = '<h1>Was that helpful?</h1>';					      	
+    			question = '<h1>Was that helpful?</h2><img src="http://images2.minutemediacdn.com/image/upload/c_fit,f_auto,fl_lossy,q_auto,w_728/v1555916687/shape/mentalfloss/cognitive_biases.png" class="centered">';					      	
     			setTimeout(timedQuestion, 2000);
 		}
 		else if (input.includes("time")) {
